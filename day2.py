2224f81156d54c7de33d4959eaa4ac1c9bf84e4f
SugerSenpai <sugerdzn@gmail.com>
2022-12-03 14:03:40 +0100

    day 2 part 1&2

---
+++ day2.py
@@ -0,0 +1,44 @@
+ROUND_DATA = "day2_input.txt"
+#this is honestly a really boring way to solve this, but it's really fast
+possibleMatchesPartOne = {
+    'A X': 4,
+    'A Y': 8,
+    'A Z': 3,
+    'B X': 1,
+    'B Y': 5,
+    'B Z': 9,
+    'C X': 7,
+    'C Y': 2,
+    'C Z': 6
+}
+
+possibleMatchesPartTwo = {
+    'A X': 3,
+    'A Y': 4,
+    'A Z': 8,
+    'B X': 1,
+    'B Y': 5,
+    'B Z': 9,
+    'C X': 2,
+    'C Y': 6,
+    'C Z': 7
+}
+
+def readFile(fileName):
+    file = open(fileName, "r")
+    lines = file.read().splitlines()
+    return lines
+
+def printArray(array):
+    for line in array:
+        print(line)
+
+def calculatePoints(matches: list, matchResults: dict):
+    sumPoints = 0
+    for match in matches:
+        sumPoints += matchResults[match]
+    return sumPoints
+
+rounds = readFile(ROUND_DATA)
+print(calculatePoints(rounds, possibleMatchesPartOne))
+print(calculatePoints(rounds, possibleMatchesPartTwo))
