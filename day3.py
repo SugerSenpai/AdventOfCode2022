95779883bbbd49bdf29b00b97d5b7f022c8dbf4f
SugerSenpai <sugerdzn@gmail.com>
2022-12-04 03:28:54 +0100

    day 3 star 1&2, deleted useless sum func

--- day3.py
+++ day3.py
@@ -1,6 +1,7 @@
-from util import readFile,printArray
+from util import readFile, printArray
 
 COMPARTMENT_DATA = "day3_input.txt"
+
 
 def getPriority(compartments: list):
     priorityList = []
@@ -9,30 +10,28 @@ def getPriority(compartments: list):
         secondPart = compartment[len(compartment)//2:]
         for char in firstPart:
             if char in secondPart:
-                priorityList += [ord(char) - 38 if ord(char) < 97 else ord(char)-96]
+                priorityList += [ord(char) - 38 if ord(char)
+                                 < 97 else ord(char)-96]
                 break
     return priorityList
 
-def getPrioritySum(priorityList: list):
-    prioritySum = 0
-    for priority in priorityList:
-        prioritySum += int(priority)
-    return prioritySum
 
 def getGroupPriority(compartments: list):
     groupPriorityList = []
-    for x in range(0, len(compartments),3):
+    for x in range(0, len(compartments), 3):
         firstRucksack = compartments[x]
         secondRucksack = compartments[x+1]
         thirdRucksack = compartments[x+2]
         for char in firstRucksack:
             if char in secondRucksack and char in thirdRucksack:
-                groupPriorityList += [ord(char) - 38 if ord(char) < 97 else ord(char)-96]
+                groupPriorityList += [ord(char) -
+                                      38 if ord(char) < 97 else ord(char)-96]
                 break
     return groupPriorityList
 
+
 rucksacks = readFile(COMPARTMENT_DATA)
 compartmentPriorityList = getPriority(rucksacks)
-print(getPrioritySum(compartmentPriorityList))
+print(sum(compartmentPriorityList))
 groupPriorityList = getGroupPriority(rucksacks)
-print(getPrioritySum(groupPriorityList))
+print(sum(groupPriorityList))
