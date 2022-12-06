b6d230ec2c95cb049537c0899c022316d04d53fb
SugerSenpai <sugerdzn@gmail.com>
2022-12-06 13:25:00 +0100

    day 6 part 1&2

---
+++ day6.py
@@ -0,0 +1,12 @@
+DATASTREAM = "day6_input.txt"
+with open(DATASTREAM, 'r') as file:
+    data = file.read().rstrip()
+
+def findMarkerPosition(datastream : str, length: int):
+    for char in range(0, len(datastream)-1, 1):
+        charSet = set(datastream[char:char+length])
+        if(len(charSet) == length):
+            return char+length
+
+print(findMarkerPosition(data, 4))
+print(findMarkerPosition(data, 14))
