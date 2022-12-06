a834cea0e57ef4beb04f12e5b22a5f96ab873700
SugerSenpai <sugerdzn@gmail.com>
2022-12-06 13:26:54 +0100

    day 6 part 1&2

--- day6.py
+++ day6.py
@@ -3,7 +3,7 @@ with open(DATASTREAM, 'r') as file:
     data = file.read().rstrip()
 
 def findMarkerPosition(datastream : str, length: int):
-    for char in range(0, len(datastream)-1, 1):
+    for char in range(0, len(datastream), 1):
         charSet = set(datastream[char:char+length])
         if(len(charSet) == length):
             return char+length
