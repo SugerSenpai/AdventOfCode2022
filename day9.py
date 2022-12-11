3ee13157b86bedcc6b2445a0f2ba86e7bdc1560d
SugerSenpai <sugerdzn@gmail.com>
2022-12-11 02:41:24 +0100

    day 9 part 2 - it was actually just adding greater than 2

--- day9.py
+++ day9.py
@@ -2,7 +2,7 @@ from util import *
 MOVE_DATA = "day9_input.txt"
 
 Head = (0,0)
-Tail = (0,0)
+Tail = [(0,0) for i in range(9)]
 moveTuples = {
     'R': (1,0),
     'L': (-1,0),
@@ -13,25 +13,25 @@ moveTuples = {
 def calculatePositions(Head, Tail):
     xDiff = Head[0] - Tail[0]
     yDiff = Head[1] - Tail[1]
-    if abs(xDiff) == 2:
+    if abs(xDiff) >= 2:
         Tail = (Head[0]+1 if Tail[0]>Head[0] else Head[0]-1, Head[1] )
-    elif abs(yDiff) == 2:
+    elif abs(yDiff) >= 2:
         Tail = (Head[0], Head[1]+1 if Tail[1]>Head[1] else Head[1]-1)
-    visitedPositions.add(Tail)
     return Tail
     
 
 moves = readFile(MOVE_DATA)
 visitedPositions = set()
+visitedPositionsTail = set()
 for move in moves:
     direction, repetitions = move.split()
     for repetition in range(int(repetitions)):
         Head = (Head[0] + moveTuples[direction][0], Head[1] + moveTuples[direction][1])
-        Tail = calculatePositions(Head,Tail)
+        Tail[0] = calculatePositions(Head,Tail[0])
+        for i in range(1,9):
+            Tail[i] = calculatePositions(Tail[i-1],Tail[i])
+        visitedPositions.add(Tail[0])
+        visitedPositionsTail.add(Tail[8])
+        
 print(len(visitedPositions))
-
-#part 2 seems actually easy to implement, but somehow i can't get it right
-#what i tried:
-#implement another 8 Tails that take the tail before them as head and add 
-#the position of the last tail to a set
-# for me it returns 2340 which should be 2562 so idk where I went wrong (deleted part 2)
+print(len(visitedPositionsTail))
